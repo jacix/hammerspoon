fb965eb12a962b770888ccf23f0540ecf50be8a8
Jason S <github@jasons.us>
2023-03-22 23:06:30 -0400

    init.lua: add menubar item to enable/disable sleep and bind a URL for other apps to call

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Jason's public hammerspoon scripts
 * init.lua - the initializer.  For now it's everything, but as I add more, I'll split things out. Contains:
   * demos from the HS intro. All are cmd-alt-ctrl: R=reload; W=notify; Q=alert; C=spoon clock
   * cmd-alt-ctrl-T = take a URL from the pasteboard and create a prettified hyperlink in Teams
+  * create menubar item to enable/disable sleep and bind URLS (hammerspoon://{stayup|chill} for other apps to call
--- init.lua
+++ init.lua
@@ -18,10 +18,17 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Q", function()
   hs.alert.show("Hello World!")
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", function()
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
+  frontmost_application = hs.application.frontmostApplication()
+  frontmost_title = frontmost_application:title()
+  hs.alert.show(frontmost_application)
+end)
+
+-- take a URL from the clipboard and make a Teams-friendly hyperlink
 -- to do:
 -- * detect whether there's a URL in the pasteboard
 -- * detect app in focus and change output accordingly
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", "Teamenheimer!", function()
   mypasteboard = hs.pasteboard.getContents()
   _, _, url, tag = string.find(mypasteboard, "(.*)/(.*)")
   hs.eventtap.keyStroke({"cmd"}, "k")
@@ -37,15 +44,58 @@ spoon.AClock:init() hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", function()
   spoon.AClock:toggleShow()
 end)
 
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
---   local win = hs.window.focusedWindow()
---   local f = win:frame()
--- 
---   f.x = f.x - 10
---   win:setFrame(f)
--- end)
+hs.hotkey.showHotkeys({"cmd", "alt", "ctrl"}, "H")
 
--- hs.hotkey.bind({"3", "3"}, "d", function()
---   hs.alert.show("Hello World!")
--- end)
+-- ###########################################################################################
+-- Create menubar item to toggle disabling of sleep
+caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+    if state then
+        caffeine:setTitle("AWAKE")
+    else
+        caffeine:setTitle("SLEEPY")
+    end
+end
 
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+
+-- bind URLs "hammerspoon://{stayup,chill}" to enable/diable sleep that can be called from scripts, etc
+hs.urlevent.bind("stayup",function(eventName, params)
+    hs.caffeinate.set("displayIdle",true)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end)
+
+hs.urlevent.bind("chill",function(eventName, params)
+    hs.caffeinate.set("displayIdle",false)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end)
+
+-- cmd-alt-ctrl-V checks whether sleep is enabled or not
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "V", function()
+  hs.alert.show(hs.caffeinate.get("displayIdle"))
+end)
+-- ###########################################################################################
+
+
+--[[ basement
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"3", "3"}, "d", function()
+  hs.alert.show("Hello World!")
+end)
+
+--]]
