77e75218c4082b4dca325f71e3a0426cba9d3f69
Jason S <github@jasons.us>
2023-03-24 23:40:57 -0400

    init.lua: add HammerText, much cleanup, add VPN check on startup for sleepifier, add vars; updated README to match

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # hammerspoon
 Jason's public hammerspoon scripts
 * init.lua - the initializer.  For now it's everything, but as I add more, I'll split things out. Contains:
-  * demos from the HS intro. All are cmd-alt-ctrl: R=reload; W=notify; Q=alert; C=spoon clock
-  * cmd-alt-ctrl-T = take a URL from the pasteboard and create a prettified hyperlink in Teams
+  * demos from the HS intro, many of which are now in the basement
+  * cmd-opt-D => launch applescript to ask for desired date pattern: dd, ddd, hh, hhh, ddhh, dddhhh
+  * ctrl-opt-cmd-C => show AClock
+  * ctrl-opt-cmd-F => show foreground application
+  * ctrl-opt-cmd-H => show active hotkeys
+  * ctrl-opt-cmd-T => take a URL from the pasteboard and create a prettified hyperlink in Teams
+  * ctrl-opt-cmd-/ => launch KSheet
   * create menubar item to enable/disable sleep and bind URLS (hammerspoon://{stayup|chill} for other apps to call
+    * ctrl-opt-cmd will report sleep status
+  * cmd-opt
+  * Spoons
+    * ClipboardTool - clipboard manager
+    * FadeLogo - fades the logo after reload
+    * HammerText - unofficial, from https://gist.github.com/maxandersen/d09ebef333b0c7b7f947420e2a7bbbf5
+    * KSheet
+    * ReloadConfiguration
+    * SpoonInstalll
--- init.lua
+++ init.lua
@@ -1,29 +1,57 @@
---[[ 
+--[[
 Jason's hammerspoon hotkeys
 
 change log:
   2023-03-20 - initial creation with R=reload, W=notify, Q=alert, C=clock
   2023-03-22 - added T=Teams linkinator
---]] 
+  2023-03-24 - added HammerText and ddd, dd, hhh, hh, ddhh; changed output and hotkey for sleep-checker
+--]]
+
 ----------------------------------------------------------------------------------------------
+-- some variables
+hyper       = {"cmd","alt","ctrl"}
+shift_hyper = {"cmd","alt","ctrl","shift"}
+ctrl_cmd    = {"cmd","ctrl"}
+-- work_logo = hs.image.imageFromPath(hs.configdir .. "/files/work_logo_2x.png")
+
+----------------------------------------------------------------------------------------------
+--[[ supplanted by ctrl_cmd-R in ReloadConfiguration spoon
 -- reload the config - simple reload
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
 end)
-hs.alert.show("Config loaded")
-
+-- hs.alert.show("Config loaded")
+--]]
 ----------------------------------------------------------------------------------------------
 -- load some spoons
-hs.loadSpoon("ReloadConfiguration")
-spoon.ReloadConfiguration:start()
+hs.loadSpoon("SpoonInstall")
+Install=spoon.SpoonInstall
 
--- hs.loadSpoon("SpoonInstall")
--- spoon.SpoonInstall:start()
+hs.loadSpoon("FadeLogo"):start()
 
-hs.loadSpoon("ClipboardTool")
-spoon.ClipboardTool:start()
+-- hs.loadSpoon("ReloadConfiguration"):start()
+Install:andUse("ReloadConfiguration", { hotkeys = { reloadConfiguration = { hyper, "R" } }})
+
+hs.loadSpoon("ClipboardTool"):start()
+
+Install:andUse("KSheet", { hotkeys = { toggle = { hyper, "/", "barf" } } })
+
+ht=hs.loadSpoon("HammerText")
+ht.keywords={
+  [ "ddd." ] = function() return os.date("%Y-%m-%d") end,
+  [ "dd." ] = function() return os.date("%Y%m%d") end,
+  [ "hhh." ] = function() return os.date("%H:%M") end,
+  [ "hh." ] = function() return os.date("%H%M") end,
+  [ "ddhh." ] = function() return os.date("%Y%m%d.%H%M") end,
+}
+
+hs.loadSpoon("AClock")
+spoon.AClock:init() hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", "A Clock", function()
+  spoon.AClock:toggleShow()
+end)
 
 ----------------------------------------------------------------------------------------------
+-- work-in-progress to get a list of windows for finding focus and such
 -- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
 --   --window_list = hs.window.list(allWindows)
 --   window_list = hs.window.allWindows()
@@ -31,13 +59,9 @@ spoon.ClipboardTool:start()
 --   -- hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 -- end)
 
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Q", function()
---   hs.alert.show("Hello World!")
--- end)
-
 ----------------------------------------------------------------------------------------------
 -- show the name of the front-most application. Good for troubleshooting.
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", "front-most application", function()
   frontmost_application = hs.application.frontmostApplication()
   frontmost_title = frontmost_application:title()
   hs.alert.show(frontmost_application)
@@ -48,7 +72,7 @@ end)
 -- to do:
 -- * detect whether there's a URL in the pasteboard
 -- * detect app in focus and change output accordingly
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", "Teamenheimer!", function()
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", "Teams link-enator", function()
   mypasteboard = hs.pasteboard.getContents()
   _, _, url, tag = string.find(mypasteboard, "(.*)/(.*)")
   hs.eventtap.keyStroke({"cmd"}, "k")
@@ -59,21 +83,16 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", "Teamenheimer!", function()
   hs.eventtap.keyStroke({}, "return")
 end)
 
-hs.loadSpoon("AClock")
-spoon.AClock:init() hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", function()
-  spoon.AClock:toggleShow()
-end)
-
 hs.hotkey.showHotkeys({"cmd", "alt", "ctrl"}, "H")
 
 ----------------------------------------------------------------------------------------------
--- Create menubar item to toggle disabling of sleep
+-- Create menubar item to toggle disabling of sleep, create URLs to call from scripts, check VPN on startup
 caffeine = hs.menubar.new()
 function setCaffeineDisplay(state)
     if state then
-        caffeine:setTitle("AWAKE")
+        caffeine:setTitle("BUZZ")
     else
-        caffeine:setTitle("SLEEPY")
+        caffeine:setTitle("zzz.")
     end
 end
 
@@ -86,7 +105,7 @@ if caffeine then
     setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
 end
 
--- bind URLs "hammerspoon://{stayup,chill}" to enable/diable sleep that can be called from scripts, etc
+-- bind URLs "hammerspoon://{stayup,chill}"
 hs.urlevent.bind("stayup",function(eventName, params)
     hs.caffeinate.set("displayIdle",true)
     setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
@@ -97,27 +116,67 @@ hs.urlevent.bind("chill",function(eventName, params)
     setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
 end)
 
--- cmd-alt-ctrl-V checks whether sleep is enabled or not
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "V", function()
-  hs.alert.show(hs.caffeinate.get("displayIdle"))
+-------
+-- ht:start() doesn't work here
+-------
+
+-- check whether sleep is enabled or not
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "S", "Is sleep disabled?", function()
+--  hs.alert.show(hs.caffeinate.get("displayIdle"))
+  if hs.caffeinate.get("displayIdle") then
+    hs.alert.show("DisplayIdle disabled. (No sleep till Brooklyn.)")
+  else
+    hs.alert.show("DisplayIdle enabled. (When is naptime?)")
+  end
 end)
+-------
+-- but ht:start() DOES work here - WTF?
+-------
+ht:start()
+
+-- Check if on VPN when HS starts, and if so, disable sleep
+-- courtesy of https://medium.com/@robhowlett/hammerspoon-the-best-mac-software-youve-never-heard-of-40c2df6db0f8
+function pingResult(object, message, seqnum, error)
+  if message == "didFinish" then
+    hs.caffeinate.set("displayIdle",true)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+    hs.alert.show("VPN detected. It's not too late, to buzz it good.")
+    hs.timer.usleep(200000)
+    avg = tonumber(string.match(object:summary(), '/(%d+.%d+)/'))
+    if avg == 0.0 then
+      hs.alert.show("No network")
+    elseif avg < 200.0 then
+      hs.alert.show("Network good (" .. avg .. "ms)")
+    elseif avg < 500.0 then
+      hs.alert.show("Network meh(" .. avg .. "ms)")
+    else
+      hs.alert.show("Network bad(" .. avg .. "ms)")
+    end
+  end
+end
+
+hs.network.ping.ping("telapp1.aws2.teladoc.com", 3, 0.2, 1.0, "any", pingResult)
+
 ----------------------------------------------------------------------------------------------
--- date-o-matic
-hs.hotkey.bind({"cmd","alt"}, "D", function()
+-- date-o-matic with hotkey cmd-alt-d
+hs.hotkey.bind({"cmd","alt"}, "D", "Datenheimer", function()
   focused_window = hs.window.focusedWindow()
   diditrun, output, codestring = hs.osascript.applescriptFromFile("./dateomatic.applescript")
   focused_window:focus()
-  hs.eventtap.keyStrokes(output)
+  if diditrun then
+    hs.eventtap.keyStrokes(output)
+  else
+    hs.alert.show("Say what?")
+  end
 end)
 
+--[[ basement: Stuff to keep as a reference
+---------- hello world --------------------
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Y", function()
   _, reply = hs.dialog.textPrompt("Main message.", "Please enter something:")
   hs.alert.show("you said "..reply)
 end)
 
---[[ basement: Stuff to keep as a reference
-
----------- hello world --------------------
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 end)
@@ -139,13 +198,8 @@ end)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
-
   f.x = f.x - 10
   win:setFrame(f)
-end)
-
-hs.hotkey.bind({"3", "3"}, "d", function()
-  hs.alert.show("Hello World!")
 end)
 
 --]]
