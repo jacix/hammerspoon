d04bbb441ef4232b011883213b4b5f7044114831
jacix <github@jasons.us>
2025-08-04 16:30:58 -0400

    hometools:tests for video editing

--- hometools.lua
+++ hometools.lua
@@ -3,8 +3,26 @@ change log:
   2024-03-06 - initial creation, extracted from init.lua - am I even using this?
   2024-03-18 - disabled in liue of music-webserver.lua
 --]]
-
 --[[
+
+-- experiment on 2025-06-05 to help with video editing
+hotkey_CmdOptY = hs.hotkey.bind({"cmd","alt"}, "Y", function()
+  -- hs.eventtap.keyStroke({"cmd"}, "y")
+  hs.eventtap.keyStroke({"cmd"}, "y", focused_app)
+  hs.timer.usleep(50000)
+  hs.eventtap.keyStroke({"cmd"}, "right")
+  hs.timer.usleep(50000)
+  hs.eventtap.keyStroke({"cmd"}, "left")
+end)
+
+
+hotkey_CmdOptY = hs.hotkey.bind({"cmd","alt"}, "Y", "TrimoTron", function()
+  print("sending a y")
+  hs.eventtap.keyStroke({"cmd"}, "y")
+  -- hs.timer.usleep(50000)
+  -- hs.eventtap.keyStroke({"cmd"}, "left")
+end)
+
 -- URLs to play and pause music
 hs.urlevent.bind("pauseMusic",function(pauseMusic, params)
   if hs.itunes.getPlaybackState() == "kPSP" then
